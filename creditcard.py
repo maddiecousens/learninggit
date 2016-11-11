8fb068dd69362f54f3d958cce53952dbba402d1d
Maddie Cousens <mscousens@gmail.com>
2016-11-11 23:53:06 +0000

    adding new files

---
+++ creditcard.py
@@ -0,0 +1,8 @@
+import os
+
+credit_card = os.environ["credit_card"]
+password = os.environ["password"]
+crush = os.environ["crush"]
+
+print "credit_card: {}, password: {}. crush: {}!!!".format(credit_card, 
+                                                           password, crush)
